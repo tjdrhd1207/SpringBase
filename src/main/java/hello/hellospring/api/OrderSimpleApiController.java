56fb0ec00ce2813e0350ea516b186eff6c384946
Hansol <whlthd1207@naver.com>
2022-04-27 10:31:37 +0900

    2022-04-27 간단한 주문 조회 V2,V3 엔티티를 DTO 로변환 - fetch조인 최적화

--- src/main/java/hello/hellospring/api/OrderSimpleApiController.java
+++ src/main/java/hello/hellospring/api/OrderSimpleApiController.java
@@ -1,13 +1,20 @@
 package hello.hellospring.api;
 
+import hello.hellospring.domain.Address;
 import hello.hellospring.domain.Order;
+import hello.hellospring.domain.OrderStatus;
 import hello.hellospring.repository.OrderJpaRepository;
 import hello.hellospring.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * xToOne(ManyToOne, OneToOne)
@@ -32,4 +39,43 @@ public class OrderSimpleApiController {
         return all;
     }
 
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto>ordersV2(){
+        //ORDER 쿼리 2번 조회됨
+        //N + 1 -> 1 + 회원 N(2) + 배송 N(2)
+        List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                                            .map(o -> new SimpleOrderDto(o))
+                                            .collect(toList());
+        return result;
+    }
+
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3(){
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+
+    @Data
+    static class SimpleOrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order){
+            orderId = order.getId();
+            name = order.getMember().getName();         //LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); //LAZY 초기화
+        }
+    }
 }
--- src/main/java/hello/hellospring/repository/OrderJpaRepository.java
+++ src/main/java/hello/hellospring/repository/OrderJpaRepository.java
@@ -67,4 +67,12 @@ public class OrderJpaRepository {
         return query.getResultList();
     }
 
+    public List<Order> findAllWithMemberDelivery() {
+        return  em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class
+        ).getResultList();
+    }
+
 }
